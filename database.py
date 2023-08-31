6ea2b18d0f297b1e8892aee2ec9b9cc84f590404
William Casey <williamcaseylucas@gmail.com>
2023-08-30 21:41:48 -0400

    took ceritifi out, added dotenv capability, and fixed time issue

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
 
-import certifi
-ca = certifi.where() # Generate certification
+load_dotenv()
+
+# import certifi
+# ca = certifi.where()  # Generate certification
 
 # url = 'mongodb://localhost:27017'
 # url = 'mongodb://root:example@localhost:27017/?authMechanism=DEFAULT'
-url = os.environ['MONGODB_URL']
+url = os.environ["MONGODB_URL"]
 # export MONGODB_URL="mongodb+srv://wrunyon3:<password>@cluster0.xa1ygkr.mongodb.net/"
 client = MongoClient(url)
 
