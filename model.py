c95209119c83a28222744258d5c116f981799e06
William Casey <williamcaseylucas@gmail.com>
2023-09-03 20:59:01 -0400

    updated backend a few days ago

--- model.py
+++ model.py
@@ -2,18 +2,21 @@ from pydantic import BaseModel, Field
 from datetime import date as date_type
 from typing import List
 
+
 class Student(BaseModel):
-  name: str
-  email: str
-  attended: int
-  missed: int
-  # present: bool
-  date: str | None # optional
-  
+    name: str
+    email: str
+    attended: int
+    missed: int
+    # present: bool
+    date: str | None  # optional
+
+
 class Attendence_Log(BaseModel):
-  email: str
-  date: str
-  
+    email: str
+    date: str
+
+
 class Teacher(BaseModel):
-  id: int
-  coords: List[float] | None
+    id: int
+    coords: List[float] | None
